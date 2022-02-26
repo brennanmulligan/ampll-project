d0faebd8713bdda9485573b6697fc6673127ef20
Scott Bucher <scott@DESKTOP-EGO2QDM.localdomain>
2022-02-26 18:12:26 -0500

    Created the migration for the auth table

---
+++ database/migrations/2022_02_26_230201_create_auth_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('auth', function (Blueprint $table) {
+            $table->string('athlete_id')->primary();
+            $table->string('refresh_token');
+            $table->string('access_token');
+            $table->timestamps();
+
+
+            // Declare our foreign key
+            $table->foreign('athlete_id')->references('athlete_id')->on('athlete')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('auth');
+    }
+};
