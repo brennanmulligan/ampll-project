180bfafe7fa20b0466b6729098b0eaa62305920a
Stqlth <stqlth@gmail.com>
2022-03-23 12:38:15 -0400

    Removed extra semi colon

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command('update:activity')->cron(config('AppConstants.activity_update_schedule'));;
+        $schedule->command('update:activity')->cron(config('AppConstants.activity_update_schedule'));
     }
 
     /**
