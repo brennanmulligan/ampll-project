67f8c1437c8e3813b822de18e25ea3cfc12048fc
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2022-03-22 23:50:18 -0400

    Added parser to parse webhook data into respective classes

--- app/Http/Middleware/JsonParser.php
+++ app/Http/Middleware/JsonParser.php
@@ -6,14 +6,18 @@ use App\Objects\Activity;
 use App\Objects\Athlete;
 use App\Objects\AuthorizationData;
 use App\Objects\RefreshData;
+use App\Objects\Updates;
+use App\Objects\WebhookData;
 
-Class JsonParser {
+class JsonParser
+{
     /**
      * A function to get the information from specific fields in a decoded JSON object
      * @param $authorizationData A decoded JSON object that we will parse in order to get the data we need to store
      * @return AuthorizationData
      */
-    function parseAuthorizationData($authorizationData) {
+    function parseAuthorizationData($authorizationData)
+    {
         $athlete = $this->parseAthleteData($authorizationData->athlete);
         return new AuthorizationData($authorizationData->access_token, $authorizationData->refresh_token, $athlete);
     }
@@ -23,7 +27,8 @@ Class JsonParser {
      * @param $refreshData A decoded JSON object that we will parse in order to get the data we need to store
      * @return RefreshData
      */
-    function parseRefreshData($refreshData) {
+    function parseRefreshData($refreshData)
+    {
         return new RefreshData($refreshData->access_token, $refreshData->refresh_token);
     }
 
@@ -32,7 +37,8 @@ Class JsonParser {
      * @param $athleteData mixed A decoded JSON object that we will parse in order to get the data we need to store
      * @return Athlete
      */
-    function parseAthleteData(mixed $athleteData) {
+    function parseAthleteData(mixed $athleteData)
+    {
         return new Athlete($athleteData->id, $athleteData->username, $athleteData->firstname, $athleteData->lastname,
             $athleteData->city, $athleteData->state, $athleteData->country, $athleteData->sex, $athleteData->updated_at);
     }
@@ -46,7 +52,7 @@ Class JsonParser {
     function parseActivitiesData(mixed $activitiesData): array
     {
         $activities = array();
-        foreach($activitiesData as $activity) {
+        foreach ($activitiesData as $activity) {
             $activities[] = new Activity($activity->id, $activity->name,
                 $activity->type, $activity->elapsed_time,
                 $activity->distance, $activity->total_elevation_gain,
@@ -54,5 +60,32 @@ Class JsonParser {
                 $activity->utc_offset, $activity->kudos_count);
         }
         return $activities;
+    }
+
+    /**
+     * @param mixed $data
+     * @return WebhookData
+     */
+    function parseWebhookData(mixed $data): WebhookData
+    {
+        $updates = new Updates();
+        if (isset($data['updates'])) {
+            // set our updates values
+            if (isset($data['updates']['title'])) {
+                $updates->setTitle($data['updates']['title']);
+            }
+            if (isset($data['updates']['type'])) {
+                $updates->setType($data['updates']['type']);
+            }
+            if (isset($data['updates']['private'])) {
+                $updates->setPrivate($data['updates']['private']);
+            }
+            if (isset($data['updates']['authorized'])) {
+                $updates->setAuthorized($data['updates']['authorized']);
+            }
+        }
+
+        return new WebhookData($data['object_type'], $data['object_id'], $data['aspect_type'], $data['owner_id'],
+            $data['subscription_id'], $data['event_time'], $updates);
     }
 }
