b567225900b5517d6a44a209a0826bb3040bca2c
Scott Bucher <47844014+scottbucher@users.noreply.github.com>
2022-03-31 18:17:14 -0400

    Work on tests

--- tests/Feature/ActivityControllerTest.php
+++ tests/Feature/ActivityControllerTest.php
@@ -15,7 +15,8 @@ class ActivityControllerTest extends TestCase
     use RefreshDatabase;
     private Activity $activity1;
     private Activity $activity2;
-    private String $id;
+    private Athlete $athlete;
+    private ActivityController $activityController;
 
     function __construct()
     {
@@ -24,17 +25,17 @@ class ActivityControllerTest extends TestCase
     }
 
     protected function setUp(): void {
-        $this->id = '37134970';
+        $this->athlete = new Athlete('37134971', 'scotbooker', 'Scott', 'Bucher',
+            'Somewhere', 'Pennsylvania', 'United States', 'M');
         $this->activity1 = new Activity('5017254429', 'Afternoon Run', 'Run', '1750',
             '5230.00', '33', '2022-12-14T21:39:47Z', '2022-12-14T17:39:47Z',
             '-14400', '0');
         $this->activity2 = new Activity('5017254430', 'Afternoon Run', 'Run', '1800',
             '5200.00', '30', '2022-12-15T21:39:47Z', '2022-12-15T17:39:47Z',
             '-14400', '0');
-        $athlete = new Athlete('37134970', 'alexschreffler', 'Alex', 'Schreffler',
-            'Herndon', 'Pennsylvania', 'United States', 'M');
+        $this->activityController = new ActivityController();
         $athleteController = new AthleteController();
-        $athleteController->addOrUpdate($athlete);
+        $athleteController->addOrUpdate($this->athlete);
     }
 
     /**
@@ -45,11 +46,20 @@ class ActivityControllerTest extends TestCase
     public function testStoreActivities()
     {
         $activities = [$this->activity1, $this->activity2];
-        $activityController = new ActivityController();
         $this->assertDatabaseMissing('activity', ['activity_id' => '5017254429']);
         $this->assertDatabaseMissing('activity', ['activity_id' => '5017254430']);
-        $activityController->storeActivities($this->id, $activities);
+        $this->activityController->storeActivities($this->athlete->getId(), $activities);
         $this->assertDatabaseHas('activity', ['activity_id' => '5017254429']);
         $this->assertDatabaseHas('activity', ['activity_id' => '5017254430']);
+    }
+
+    public function testGetAllActivitiesForAthlete() {
+        $activities = [$this->activity1, $this->activity2];
+        $this->assertDatabaseMissing('activity', ['athlete_id' => $this->athlete->getId()]);
+
+        $this->activityController->storeActivities($this->athlete->getId(), $activities);
+
+        $this->assertDatabaseHas('activity', ['activity' => $activities[0]->getId()]);
+        $this->assertDatabaseHas('activity', ['athlete_id' => $activities[1]->getId()]);
     }
 }
